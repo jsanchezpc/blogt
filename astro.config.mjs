10636138c8a8b216563895fe5ea76cf1dd8b674c
jsanchezpc <jsanchezpc@hotmail.com>
2023-10-15 00:39:23 +0200

    deleted base const from all files

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://pages.github.com/jsanchezpc',
-	base: '/blogt',
+	site: 'https://jsanchezpc.github.io/',
+	// base: '',
 	integrations: [mdx(), sitemap()],
 });
